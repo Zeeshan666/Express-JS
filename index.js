2f7e01ce94375a106cf595bc107d0484e19b7e96
zeeshan666 <zeeshanmanzoor.zm@gmail.com>
2019-12-27 04:10:06 -0800

    express routes

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require("express"),
   http = require("http");
-
+const dishRouter = require("./routes/dishRouters");
 const morgan = require("morgan");
-
 
 const hostname = "localhost";
 const port = 3000;
@@ -11,49 +10,8 @@ const app = express();
 
 app.use(morgan("dev"));
 app.use(bodyParser.json());
+app.use("/dishes", dishRouter);
 
-app.all("/dishes", (req, res, next) => {
-  res.statusCode = 200;
-  res.setHeader = ("application/type", "html/txt");
-  next();
-});
-app.get("/dishes", (req, res, next) => {
-  res.end("we will send you all dishes");
-});
-app.post("/dishes", (req, res, next) => {
-  res.end(
-    "Will add the dish: " +
-      req.body.name +
-      " with details: " +
-      req.body.description
-  );
-});
-app.put("/dishes", (req, res, next) => {
-  res.statusCode = 403;
-  res.end("not supported in dishes");
-});
-app.delete("/dishes", (req, res, next) => {
-  res.end("all items will delete");
-});
-app.get("/dishes/:dishID", (req, res, next) => {
-  res.end("we will send you requested dish" + req.params.dishID + "to you");
-});
-app.post("/dishes/:dishID", (req, res, next) => {
-  res.statusCode = 403;
-  res.end(" post  not supported in dishes" + req.params.dishID + "");
-});
-app.put("/dishes/:dishID", (req, res, next) => {
-  res.write("updating the dish" + req.params.dishID + "\n");
-  res.end(
-    "will update the dish" +
-      req.body.name +
-      "with details" +
-      req.body.discription
-  );
-});
-app.delete("/dishes/:dishID", (req, res, next) => {
-  res.end("this items will delete " + req.params.dishID);
-});
 app.use(express.static(__dirname + "/public"));
 app.use((req, res, next) => {
   //   console.log(req.headers);
---
+++ routes/dishRouters.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const dishRouter = express.Router();
+dishRouter.use(bodyParser.json());
+dishRouter
+  .route("/")
+  .all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader = ("application/type", "html/txt");
+    next();
+  })
+  .get((req, res, next) => {
+    res.end("we will send you all dishes");
+  })
+  .post((req, res, next) => {
+    res.end(
+      "Will add the dish: " +
+        req.body.name +
+        " with details: " +
+        req.body.description
+    );
+  })
+  .put((req, res, next) => {
+    res.statusCode = 403;
+    res.end("not supported in dishes");
+  })
+  .delete((req, res, next) => {
+    res.end("all items will delete");
+  });
+// .get("/dishes/:dishID", (req, res, next) => {
+//   res.end("we will send you requested dish" + req.params.dishID + "to you");
+// });
+// app.post("/dishes/:dishID", (req, res, next) => {
+//   res.statusCode = 403;
+//   res.end(" post  not supported in dishes" + req.params.dishID + "");
+// });
+// app.put("/dishes/:dishID", (req, res, next) => {
+//   res.write("updating the dish" + req.params.dishID + "\n");
+//   res.end(
+//     "will update the dish" +
+//       req.body.name +
+//       "with details" +
+//       req.body.discription
+//   );
+// });
+// app.delete("/dishes/:dishID", (req, res, next) => {
+//   res.end("this items will delete " + req.params.dishID);
+// });
+
+
+module.exports=dishRouter;
